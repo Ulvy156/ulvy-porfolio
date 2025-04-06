8279b6b2a1b32f19fb2f6dd2e0010b6bbf05328a
Ulvy156 <ulvyromy156@gmail.com>
2025-04-06 15:04:39 +0700

    Add form submission handler and update footer content with current year

--- components/FeatureProjects.vue
+++ components/FeatureProjects.vue
@@ -26,8 +26,7 @@
             </div>
             <div class="flex items-center gap-x-2">
                 <p>View details</p>
-          <svg xmlns="http://www.w3.org/2000/svg" class="size-5" viewBox="0 0 24 24"><path fill="currentColor" d="M4 12h12.25L11 6.75l.66-.75l6.5 6.5l-6.5 6.5l-.66-.75L16.25 13H4z"/></svg>
-
+                <svg xmlns="http://www.w3.org/2000/svg" class="size-5" viewBox="0 0 24 24"><path fill="currentColor" d="M4 12h12.25L11 6.75l.66-.75l6.5 6.5l-6.5 6.5l-.66-.75L16.25 13H4z"/></svg>
             </div>
         </div>
      </section>
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-    <footer class="w-11/12 m-auto ">
-        <p class="text-white">All Right Reserve</p>
+    <footer class="w-11/12 m-auto border-t border-gray-500 p-3 text-center mb-5">
+        <p class="text-white">@ {{ year }} Ulvy. All Right Reserved.</p>
     </footer>
 </template>
+
+<script setup lang="ts">
+
+const year = new Date().getFullYear();
+
+</script>
