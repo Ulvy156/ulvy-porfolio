9b478f236e23b9db412decb20ed9a546877718c3
Ulvy156 <ulvyromy156@gmail.com>
2025-04-06 14:16:04 +0700

    Adjust backtop positioning in app.vue for improved UI layout

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
     <NuxtPage />
   </main>
   <Footer />
-  <el-backtop :right="100" :bottom="100" >
+  <el-backtop :right="10" :bottom="20" >
     <template #default>
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m6 11l6-6l6 6M6 19l6-6l6 6"/></svg>
     </template>
